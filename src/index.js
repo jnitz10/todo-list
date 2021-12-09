4f871a9a00dc6843d36bdc211a74ddb021fb4db6
Jnitz10 <jnitz10@gmail.com>
2021-12-08 20:54:55 -0700

    commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
-console.log("sdf");
+import { formatDistance, subDays, addDays } from "date-fns";
+
+
+const todoItem = (title, description, dueDate, priority) => {
+    return { title, description, dueDate, priority };
+};
+
+const asdf = todoItem('store', 'go to store', 'tuesday', 'urgent');
+
+let myTasks = [];
+
+myTasks.push(asdf);
+
+/*
+CODE TO REFACTOR
+*/
+
+function addTask() {
+    var task = todoItem(
+        document.getElementById("taskTitle").value,
+        document.getElementById("taskDescription").value,
+        document.getElementById("dueDate").value,
+        document.getElementById("priority").value
+        );
+        myTasks.push(task);
+}
+
+document.getElementById('addTaskButton').addEventListener('click', function() {
+    document.querySelector('.bg-modal').style.display = 'flex';
+})
+
+document.querySelector('.close').addEventListener('click', function() {
+    document.querySelector('.bg-modal').style.display = 'none';
+})
+
+document.getElementById('formButton').addEventListener('click', function() {
+    addTask();
+    console.log(myTasks)
+    document.getElementById('taskForm').reset();
+});
